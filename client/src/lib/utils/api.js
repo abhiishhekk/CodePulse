88e78763727f6f32c9ee59c86497b22639568ff6
Abhishek Kumar <abhishekkumar736993@gmail.com>
2025-09-08 00:44:26 +0530

    url updated

--- client/src/lib/utils/api.js
+++ client/src/lib/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/v1/", // your backend proxy
+  baseURL: "https://leetpulse.onrender.com/api/v1/", // your backend proxy
   timeout: 5000,
 });
 
