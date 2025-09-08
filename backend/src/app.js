eb3de47e9fd78a0bdd4c0c989fa2546c35710c3f
Abhishek Kumar <abhishekkumar736993@gmail.com>
2025-09-08 09:40:55 +0530

    updated cors origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,20 +8,31 @@ import router from "./routes/index.js";
 
 const app = express();
 console.log("FRONTEND URL", process.env.FRONTENDURL);
+const allowedOrigins = [
+    process.env.FRONTENDURL, // Your deployed frontend
+    'http://localhost:5173', // Your local development frontend
+];
 app.use(cors(
     {
-        origin: process.env.FRONTENDURL,
+        origin: (origin, callback) => {
+            // Allow requests from the allowed origins, and also non-browser requests (like from Postman)
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error('Not allowed by CORS'));
+            }
+        },
         methods: ["GET", "POST"],
         credentials: true,
     }
 ))
-app.use(express.json({limit:"16kb"})) //ye isliye ki hum json request accept krenge
+app.use(express.json({ limit: "16kb" })) //ye isliye ki hum json request accept krenge
 //jaise ki forms
 
 // ab url se data aye toh kaise krna hai
 app.use(express.urlencoded({
-    extended:true,
-    limit:"16kb"
+    extended: true,
+    limit: "16kb"
 }))
 app.use(express.static("public"))
 
